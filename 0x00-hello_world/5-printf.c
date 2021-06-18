a289cc83af58d189cda683fb41ed6ef7f8f9352e
tatianajimenez <jimenezguzmant@gmail.com>
2021-06-17 19:19:12 -0500

    other-art

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - This is a description
+ * Return: print Programming is like building a multilingual puzzle
+ */
+int main(void)
+{
+char text2[] = "with proper grammar, but the outcome is a piece of art,";
+printf(text2);
+return (0);
+}
+
