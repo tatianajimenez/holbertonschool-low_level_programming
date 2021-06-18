b6649b3cf2e9d5ea49c72101447532143149f961
tatianajimenez <jimenezguzmant@gmail.com>
2021-06-17 19:29:04 -0500

    other-out

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 /**
  * main - This is a description
  * Return: print Programming is like building a multilingual puzzle
  */
 int main(void)
 {
-char text2[] = "with proper grammar, but the outcome is a piece of art,";
-printf(text2);
+char text2[100] = "with proper grammar, but the outcome is a piece of art,\n";
+printf("%s", text2);
 return (0);
 }
-
