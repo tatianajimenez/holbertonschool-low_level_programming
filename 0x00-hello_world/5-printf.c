6592b9e0268e91bd6c60028f7973127758d1c137
tatianajimenez <jimenezguzmant@gmail.com>
2021-06-20 17:55:14 -0500

    positive-commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
-#include <string.h>
-/**
- * main - This is a description
- * Return: print Programming is like building a multilingual puzzle
- */
+
 int main(void)
 {
-char text2[100] = "with proper grammar, but the outcome is a piece of art,\n";
-printf("%s", text2);
-return (0);
+    char text2[100] = "with proper grammar, but the outcome is a piece of art,\n";
+    printf("%s", text2);
+    return (0);
 }
